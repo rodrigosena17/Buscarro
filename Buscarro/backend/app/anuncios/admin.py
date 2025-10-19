5d259b2f5040b0b86139863a13e30ab004f5c115
Karen Ribeiro <kevr@discente.ifpe.edu.br>
2025-10-19 20:37:09 -0300

    feat: adiciona models admin de usuario e anuncio

--- Buscarro/backend/app/anuncios/admin.py
+++ Buscarro/backend/app/anuncios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Anuncio
 
 # Register your models here.
+@admin.register(Anuncio)
+class AnuncioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'marca', 'modelo', 'ano', 'preco', 'origem', 'is_active')
+    list_filter = ('marca', 'ano', 'origem', 'is_active')
+    search_fields = ('titulo', 'marca', 'modelo', 'descricao', 'localizacao', 'autor_nome')
+    ordering = ('-id',)
+
